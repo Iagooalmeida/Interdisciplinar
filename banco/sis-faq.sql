bece5343358ee63ba2c54a318d8050fcf09b7611
Iagooalmeida <iago732@gmail.com>
2023-11-24 18:54:14 -0300

    Atualizando script sql banco

--- banco/sis-faq.sql
+++ banco/sis-faq.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21/11/2023 às 22:51
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.0.28
+-- Tempo de geração: 24/11/2023 às 22:52
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,10 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `atualizacoes` (
   `idAtualizacao` int(10) UNSIGNED NOT NULL,
-  `Usuarios_idUsuarios` int(10) UNSIGNED NOT NULL,
-  `perguntas_idPerguntas` int(10) UNSIGNED NOT NULL,
-  `DataAtualizacao` datetime NOT NULL
+  `atualizar_fk_idUsuarios` int(10) UNSIGNED NOT NULL,
+  `atualizar_fk_idPerguntas` int(10) UNSIGNED NOT NULL,
+  `DataAtualizacao` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `atualizacoes`
+--
+
+INSERT INTO `atualizacoes` (`idAtualizacao`, `atualizar_fk_idUsuarios`, `atualizar_fk_idPerguntas`, `DataAtualizacao`) VALUES
+(6, 1, 6, '2023-11-24 17:54:10');
 
 -- --------------------------------------------------------
 
@@ -46,22 +53,23 @@ CREATE TABLE `perguntas` (
   `temas_idTemas` int(10) UNSIGNED NOT NULL,
   `Autor` varchar(30) DEFAULT NULL,
   `ConteudoPergunta` text NOT NULL,
-  `Resposta` text,
+  `Resposta` text DEFAULT NULL,
   `Status` varchar(50) DEFAULT 'Pendente',
   `DataSubmissao` datetime NOT NULL DEFAULT current_timestamp(),
   `UltimaAtualizacao` datetime DEFAULT NULL,
-  `Visivel` tinyint(1) DEFAULT 1
+  `Origem` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Despejando dados para a tabela `perguntas`
 --
 
-INSERT INTO `perguntas` (`idPerguntas`, `Usuarios_idUsuarios`, `temas_idTemas`, `Autor`, `ConteudoPergunta`, `Resposta`, `Status`, `DataSubmissao`, `UltimaAtualizacao`, `Visivel`) VALUES
-(3, 1, 2, 'Master', 'Como fazer a rematrícula ?', 'Para obter o horário de aula atualizado da Fatec de Itapira, aconselho acessar o site oficial da instituição. Lá, você encontrará informações detalhadas sobre os horários de aula de cada curso oferecido, bem como outras informações relevantes sobre a instituição. \r\nÉ sempre recomendável verificar o site da Fatec de Itapira regularmente, pois o horário de aula pode ser atualizado a cada semestre ou período letivo.', 'Aprovado', '2023-11-10 04:02:50', NULL, 1),
-(4, 1, 2, 'Master', 'Como faço para pedir um atestado?', 'Os atestados devem ser solicitados no sistema SIGA (Sistema Integrado de Gestão Acadêmica), na opção Solicitação de Documentos.\r\n\r\nApós 3 dias úteis o atestado ficará disponível na tela de Solicitação de Documentos e também será enviado para o aluno na plataforma Ms Teams.', 'Aprovado', '2023-11-11 01:28:39', '2023-11-11 01:30:05', 1),
-(5, 1, 2, 'Master', 'Como faço para trancar minha matrícula?', 'De acordo com o artigo 35 do Regulamento Geral dos Cursos das Fatecs, o aluno veterano tem a opção de fazer até dois trancamentos de matrícula, seja de forma consecutiva ou não. Para solicitar o trancamento, o aluno deve comparecer à Secretaria Acadêmica da Unidade com a sua carteirinha e preencher um requerimento de solicitação de trancamento. É importante observar que o pedido de trancamento de matrícula só pode ser realizado dentro do prazo final estipulado no Calendário Acadêmico, disponível no site da Fatec de Itapira. Link abaixo:', 'Aprovado', '2023-11-14 17:12:57', NULL, 1),
-(6, 2, 1, 'Tux Linux', 'Como faço para ver o meu horário de aula?', 'Aguardando Resposta', 'Pendente', '2023-11-14 17:17:44', NULL, 1);
+INSERT INTO `perguntas` (`idPerguntas`, `Usuarios_idUsuarios`, `temas_idTemas`, `Autor`, `ConteudoPergunta`, `Resposta`, `Status`, `DataSubmissao`, `UltimaAtualizacao`, `Origem`) VALUES
+(3, 1, 2, 'Master', 'Como fazer a rematrícula ?', 'Para obter o horário de aula atualizado da Fatec de Itapira, aconselho acessar o site oficial da instituição. Lá, você encontrará informações detalhadas sobre os horários de aula de cada curso oferecido, bem como outras informações relevantes sobre a instituição. \r\nÉ sempre recomendável verificar o site da Fatec de Itapira regularmente, pois o horário de aula pode ser atualizado a cada semestre ou período letivo.', 'Aprovado', '2023-11-10 04:02:50', NULL, 'Usuário'),
+(4, 1, 2, 'Master', 'Como faço para pedir um atestado?', 'Os atestados devem ser solicitados no sistema SIGA (Sistema Integrado de Gestão Acadêmica), na opção Solicitação de Documentos.\r\n\r\nApós 3 dias úteis o atestado ficará disponível na tela de Solicitação de Documentos e também será enviado para o aluno na plataforma Ms Teams.', 'Aprovado', '2023-11-11 01:28:39', '2023-11-11 01:30:05', 'Usuário'),
+(5, 1, 2, 'Master', 'Como faço para trancar minha matrícula?', 'De acordo com o artigo 35 do Regulamento Geral dos Cursos das Fatecs, o aluno veterano tem a opção de fazer até dois trancamentos de matrícula, seja de forma consecutiva ou não. Para solicitar o trancamento, o aluno deve comparecer à Secretaria Acadêmica da Unidade com a sua carteirinha e preencher um requerimento de solicitação de trancamento. É importante observar que o pedido de trancamento de matrícula só pode ser realizado dentro do prazo final estipulado no Calendário Acadêmico, disponível no site da Fatec de Itapira. Link abaixo:', 'Aprovado', '2023-11-14 17:12:57', NULL, 'Usuário'),
+(6, 2, 1, 'Tux Linux', 'Como faço para ver o meu horário de aula?', 'acessando a pagina do site fatec itapira e indo no menu academico', 'Pendente', '2023-11-14 17:17:44', '2023-11-24 17:54:10', 'Usuário'),
+(9, NULL, 3, 'user', 'Como eu faço para me escrever no intercambio ?', 'Aguardando Resposta', 'Pendente', '2023-11-23 17:52:55', NULL, 'Visitante');
 
 -- --------------------------------------------------------
 
@@ -79,6 +87,13 @@ CREATE TABLE `sugestoes` (
   `DataSubmissao` datetime NOT NULL DEFAULT current_timestamp(),
   `idTema` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `sugestoes`
+--
+
+INSERT INTO `sugestoes` (`idSugestao`, `Nome`, `Email`, `Telefone`, `ConteudoSugestao`, `Status`, `DataSubmissao`, `idTema`) VALUES
+(1, 'user', 'user@gmail.com', '(97) 98787-9894', 'Como eu faço para me escrever no intercambio ?', 'Pendente', '2023-11-23 17:52:55', 3);
 
 -- --------------------------------------------------------
 
@@ -115,7 +130,7 @@ CREATE TABLE `usuarios` (
   `Tel_Cel` varchar(15) DEFAULT NULL,
   `Funcao` varchar(30) DEFAULT NULL,
   `NivelAcesso` int(11) NOT NULL DEFAULT 1,
-  `DataCadastro` date NOT NULL,
+  `DataCadastro` date NOT NULL DEFAULT curdate(),
   `DataUltimaAtualizacao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `FotoPath` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
@@ -128,7 +143,7 @@ INSERT INTO `usuarios` (`idUsuarios`, `NomeUsuario`, `Email`, `Senha`, `Tel_Cel`
 (1, 'Master', 'master@master.com', '$2y$10$9h6mcR.7yI7.vFsNfWx4vOVcpbuovwwIYnTI9jjxpn3QcGqnPaCOi', '(19) 9999-9999', 'Administrador', 0, '2023-11-11', '2023-11-11 02:14:09', NULL),
 (2, 'Tux Linux', 'user@gmail.com', '$2y$10$4/ZVhacUkpM4Kdx.U5ykMOQmiWjTZXKBh.4r6U3SdTqkIlwzB4yrC', '', 'Coordenador', 1, '2023-11-11', '2023-11-14 09:26:35', 'C:\\xampp\\htdocs\\Teste\\Controllers/../uploads/c8541bcff461b1d589d67cc503570c4c.png'),
 (3, 'User', 'user.01@gmail.com', '$2y$10$TQlLfiQ4TcyVXPzAU0W1cuTPvPCkjzW/PFwytaVenRrepyppfUKhi', '(19) 9999-9999', 'Administrador', 1, '2023-11-11', '2023-11-11 09:52:03', 'C:\\xampp\\htdocs\\Teste\\Controllers/../uploads/3bbb2337b185347c6827254600d741f5.jpg'),
-(4, 'User 02', 'User.03@gmail.com', '$2y$10$lFVqX3hB.lNb9Dh2tedHzuaGgFl3E19f2x3Pw.pO74eX7jyKumIXK', '', 'Administrador', 1, '2023-11-11', '2023-11-11 03:45:23', '');
+(4, 'User 02', 'User.03@gmail.com', '$2y$10$lFVqX3hB.lNb9Dh2tedHzuaGgFl3E19f2x3Pw.pO74eX7jyKumIXK', '(78) 96416-5368', 'Administrador', 1, '2023-11-11', '2023-11-24 10:15:43', '');
 
 --
 -- Índices para tabelas despejadas
@@ -139,8 +154,8 @@ INSERT INTO `usuarios` (`idUsuarios`, `NomeUsuario`, `Email`, `Senha`, `Tel_Cel`
 --
 ALTER TABLE `atualizacoes`
   ADD PRIMARY KEY (`idAtualizacao`),
-  ADD KEY `idx_atualizacoes_perguntas` (`perguntas_idPerguntas`),
-  ADD KEY `idx_atualizacoes_usuarios` (`Usuarios_idUsuarios`);
+  ADD KEY `idx_atualizacoes_perguntas` (`atualizar_fk_idPerguntas`),
+  ADD KEY `idx_atualizacoes_usuarios` (`atualizar_fk_idUsuarios`);
 
 --
 -- Índices de tabela `perguntas`
@@ -179,19 +194,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `atualizacoes`
 --
 ALTER TABLE `atualizacoes`
-  MODIFY `idAtualizacao` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idAtualizacao` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `perguntas`
 --
 ALTER TABLE `perguntas`
-  MODIFY `idPerguntas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idPerguntas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `sugestoes`
 --
 ALTER TABLE `sugestoes`
-  MODIFY `idSugestao` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idSugestao` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `temas`
@@ -203,7 +218,7 @@ ALTER TABLE `temas`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuarios` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idUsuarios` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Restrições para tabelas despejadas
@@ -213,8 +228,8 @@ ALTER TABLE `usuarios`
 -- Restrições para tabelas `atualizacoes`
 --
 ALTER TABLE `atualizacoes`
-  ADD CONSTRAINT `atualizacoes_ibfk_1` FOREIGN KEY (`perguntas_idPerguntas`) REFERENCES `perguntas` (`idPerguntas`) ON DELETE CASCADE,
-  ADD CONSTRAINT `atualizacoes_ibfk_2` FOREIGN KEY (`Usuarios_idUsuarios`) REFERENCES `usuarios` (`idUsuarios`);
+  ADD CONSTRAINT `atualizacoes_ibfk_1` FOREIGN KEY (`atualizar_fk_idPerguntas`) REFERENCES `perguntas` (`idPerguntas`) ON DELETE CASCADE,
+  ADD CONSTRAINT `atualizacoes_ibfk_2` FOREIGN KEY (`atualizar_fk_idUsuarios`) REFERENCES `usuarios` (`idUsuarios`) ON DELETE CASCADE;
 
 --
 -- Restrições para tabelas `perguntas`
